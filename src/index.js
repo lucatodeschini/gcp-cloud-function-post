8793886513924e9f74ee9c9217bc4127f32c4eed
Luca Todeschini <luca.todeschini@ingka.ikea.com>
2021-07-06 07:39:15 +0200

    start handling key value input

--- src/index.js
+++ src/index.js
@@ -5,11 +5,17 @@ const {getAuthConfigHeader} = require("./utils");
 
 async function main() {
     const cloudFunctionUrl = core.getInput('cloud_function_url', { required: true });
-    const jsonBody = core.getInput('json_body', { required: true });
+    const jsonBody = core.getInput('json_body', { required: false });
+    const keyValue = core.getInput('key_value', { required: false });
 
     const config = await getAuthConfigHeader(cloudFunctionUrl,'key.json')
 
-    const message = JSON.parse(jsonBody)
+    let message = {}
+    if(jsonBody){
+        message = JSON.parse(jsonBody)
+    }else{
+        core.setOutput('parsed_input', keyValue)
+    }
 
     axios.post(cloudFunctionUrl, message, config)
          .then((response) => {
